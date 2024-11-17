d76f8873810104f7a71e12c2a6db8ac6f0f86ff1
AlessandroMangili <alessandro.mangili1@studenti.unimi.it>
2024-11-17 21:44:45 +0100

    Refactoring of the code

--- src/second_node.cpp
+++ src/second_node.cpp
@@ -8,7 +8,10 @@
 #include <string>
 #include <math.h>
 
-const float THR = 1.0;  // Threshold to avoid collision between the two turtles
+const float THR = 1.0;      // Threshold to avoid collision between the two turtles
+const double MIN = 1.0;     // Minimum threshold to avoid collisions with the edges
+const double MAX = 10.0;    // Maximum threshold to avoid collisions with the edges
+
 double x[2] = {5.544445, 2}, y[2] = {5.544445, 1}, z[2] = {0, 0};
 double x_vel[2], y_vel[2], z_vel[2];    // Contain the speed of the two turtles
 bool is_moving_t1 = false;
@@ -46,6 +49,20 @@ void turtlesim2CallbackVel(const geometry_msgs::Twist::ConstPtr& msg) {
     y_vel[1] = msg->linear.y;
     z_vel[1] = msg->angular.z;
 }
+// Stop the moving turtle by setting the velocity to zero
+void stop_turtle(ros::Publisher& turtle_pub, geometry_msgs::Twist turtle_vel) {
+    turtle_vel.linear.x = 0;
+    turtle_vel.linear.y = 0;
+    turtle_vel.angular.z = 0;
+    turtle_pub.publish(turtle_vel);
+}
+// Send the turtle backward by publishing the opposite velocity
+void reverse_velocity(int id, ros::Publisher& turtle_pub, geometry_msgs::Twist turtle_vel) {
+    turtle_vel.linear.x = -x_vel[id];
+    turtle_vel.linear.y = -y_vel[id];
+    turtle_vel.angular.z = -z_vel[id];
+    turtle_pub.publish(turtle_vel);
+}
 
 int main(int argc, char **argv){
     ros::init(argc, argv, "turtlesim_listener2");
@@ -68,7 +85,6 @@ int main(int argc, char **argv){
     geometry_msgs::Twist turtle1_vel, turtle2_vel;
     ros::Time start;
     std_msgs::Float32 distance;
-    double last_stop_distance;
     while (ros::ok()) {
         // Calculate the relative distance between the two turtles and publish it on the topic
         distance.data = sqrt(pow(x[0] - x[1], 2) + pow(y[0] - y[1], 2));
@@ -76,109 +92,47 @@ int main(int argc, char **argv){
 
         // First check: verify that the relative distance is not less than or equal to the threshold, otherwise stop the turtle that is moving
         if (distance.data <= THR) {
-            int which_turtle;
             // Find the turtle that is moving and stop it by publishing its speed as zero
-            switch (is_moving_t1) {
-                case true:
-                    which_turtle = 1;
-                    turtle1_vel.linear.x = 0;
-                    turtle1_vel.linear.y = 0;
-                    turtle1_vel.angular.z = 0;
-                    break;
-                case false:
-                    which_turtle = 2;
-                    turtle2_vel.linear.x = 0;
-                    turtle2_vel.linear.y = 0;
-                    turtle2_vel.angular.z = 0;
-                    break;
-            }
+            int which_turtle = is_moving_t1 ? 1 : 2;
             ROS_INFO("Stop turtle %d because it's too close to the other turtle [distance: %f]: %f, %f, %f - %f, %f, %f", which_turtle, distance.data, x[0], y[0], z[0], x[1], y[1], z[1]);
-            which_turtle == 1 ? turtle1_pub.publish(turtle1_vel) : turtle2_pub.publish(turtle2_vel);
-
+            // Stop the moving turtle by setting the velocity to zero
+            stop_turtle(is_moving_t1 ? turtle1_pub : turtle2_pub, is_moving_t1 ? turtle1_vel : turtle2_vel);
+    
             // Set the speeds in reverse to make it go back for 0.1 seconds from where it came
-            switch (which_turtle) {
-                case 1:
-                    turtle1_vel.linear.x = -x_vel[0];
-                    turtle1_vel.linear.y = -y_vel[0];
-                    turtle1_vel.angular.z = -z_vel[0];
-                    break;
-                case 2:
-                    turtle2_vel.linear.x = -x_vel[1];
-                    turtle2_vel.linear.y = -y_vel[1];
-                    turtle2_vel.angular.z = -z_vel[1];
-                    break;
-            }
-            which_turtle == 1 ? turtle1_pub.publish(turtle1_vel) : turtle2_pub.publish(turtle2_vel);
+            reverse_velocity(which_turtle - 1, which_turtle == 1 ? turtle1_pub : turtle2_pub, which_turtle == 1 ? turtle1_vel : turtle2_vel);
             ros::spinOnce();
             ros::Duration(0.1).sleep();
 
             // Stop the turtle's backward movement by publishing its speed as zero
-            switch (which_turtle) {
-                case 1:
-                    turtle1_vel.linear.x = 0;
-                    turtle1_vel.linear.y = 0;
-                    turtle1_vel.angular.z = 0;
-                    break;
-                case 2:
-                    turtle2_vel.linear.x = 0;
-                    turtle2_vel.linear.y = 0;
-                    turtle2_vel.angular.z = 0;
-                    break;
-            }
-            which_turtle == 1 ? turtle1_pub.publish(turtle1_vel) : turtle2_pub.publish(turtle2_vel);
-            
-            /*distance.data = sqrt(pow(x[0] - x[1], 2) + pow(y[0] - y[1], 2));
-            last_stop_distance = distance.data;*/
+            stop_turtle(which_turtle == 1 ? turtle1_pub : turtle2_pub, which_turtle == 1 ? turtle1_vel : turtle2_vel);
         }
-        if (x[0] > 10.0 || x[0] < 1.0 || y[0] > 10.0 || y[0] < 1.0/*&& distance.data != last_stop_distance*/) {
+        if (x[0] > MAX || x[0] < MIN || y[0] > MAX || y[0] < MIN) {
             // Stop the turtle 1 by publishing its speed as zero
             ROS_INFO("Stop turtle 1 because it's too close to the edge: %f, %f, %f", x[0], y[0], z[0]);
-            turtle1_vel.linear.x = 0;
-            turtle1_vel.linear.y = 0;
-            turtle1_vel.angular.z = 0;
-            turtle1_pub.publish(turtle1_vel);
+            // Stop the moving turtle by setting the velocity to zero
+            stop_turtle(turtle1_pub, turtle1_vel);
             
             // Set the speeds in reverse to make it go back for 0.1 seconds from where it came
-            turtle1_vel.linear.x = -x_vel[0];
-            turtle1_vel.linear.y = -y_vel[0];
-            turtle1_vel.angular.z = -z_vel[0];
-            turtle1_pub.publish(turtle1_vel);
+            reverse_velocity(0, turtle1_pub, turtle1_vel);
             ros::spinOnce();
             ros::Duration(0.1).sleep();
 
             // Stop the turtle's backward movement by publishing its speed as zero
-            turtle1_vel.linear.x = 0;
-            turtle1_vel.linear.y = 0;
-            turtle1_vel.angular.z = 0;
-            turtle1_pub.publish(turtle1_vel);
-
-            /*distance.data = sqrt(pow(x[0] - x[1], 2) + pow(y[0] - y[1], 2));
-            last_stop_distance = distance.data;*/
+            stop_turtle(turtle1_pub, turtle1_vel);
         } 
-        if (x[1] > 10.0 || x[1] < 1.0 || y[1] > 10.0 || y[1] < 1.0/* && distance.data != last_stop_distance*/) {
+        if (x[1] > MAX || x[1] < MIN || y[1] > MAX || y[1] < MIN) {
             // Stop the turtle 2 by publishing its speed as zero
             ROS_INFO("Stop turtle 2 because it's too close to the edge: %f, %f, %f", x[1], y[1], z[1]);
-            turtle2_vel.linear.x = 0;
-            turtle2_vel.linear.y = 0;
-            turtle2_vel.angular.z = 0;
-            turtle2_pub.publish(turtle2_vel);
+            // Stop the moving turtle by setting the velocity to zero
+            stop_turtle(turtle2_pub, turtle2_vel);
 
             // Set the speeds in reverse to make it go back for 0.1 seconds from where it came
-            turtle2_vel.linear.x = -x_vel[1];
-            turtle2_vel.linear.y = -y_vel[1];
-            turtle2_vel.angular.z = -z_vel[1];
-            turtle2_pub.publish(turtle2_vel);
+            reverse_velocity(1, turtle2_pub, turtle2_vel);
             ros::spinOnce();
             ros::Duration(0.1).sleep();
 
             // Stop the turtle's backward movement by publishing its speed as zero
-            turtle2_vel.linear.x = 0;
-            turtle2_vel.linear.y = 0;
-            turtle2_vel.angular.z = 0;
-            turtle2_pub.publish(turtle2_vel);
-
-            /*distance.data = sqrt(pow(x[0] - x[1], 2) + pow(y[0] - y[1], 2));
-            last_stop_distance = distance.data;*/
+            stop_turtle(turtle2_pub, turtle2_vel);
         }
         ros::spinOnce();
         loop_rate.sleep();
